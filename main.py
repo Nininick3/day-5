fe512b7594b9af980e8945e445da3d5728930956
Nick <kolyakoluch@gmail.com>
2022-12-24 18:15:09 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def fir():
+    print('      *')
+    print('     / \ ')
+    print('    /   \ ')
+    print('   /     \ ')
+    print('  /       \ ')
+    print(' /         \ ')
+    print('/           \ ')
+    print('--------------')
+    print('     |  |       ')
+    print('     |  |       ')
+    print('     ----')
+
+if __name__ == '__main__':
+    fir()
+
